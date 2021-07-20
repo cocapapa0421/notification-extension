f25dedc96036bae129ffb0bfb9969d7e779b9fb4
cocapapa0421 <nvhuy.dev@gmail.com>
2021-07-20 09:25:29 +0700

    Them thong bao dang ky thanh cong, nut dang ky lai cho popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,22 +1,95 @@
-const formElement = document.getElementById('form');
-formElement.addEventListener('submit', registerSupporter);
+'use strict';
+const rootEl = document.getElementById('root');
 
 function registerSupporter(event) {
   event.preventDefault();
-  const inputNameElement = document.querySelector('.inputName');
-  const inputPositionElement = document.querySelector('.inputPosition');
+  chrome.storage.sync.get('wilSupportStaff', ({ wilSupportStaff: staff }) => {
+    if (!!staff) {
+      return;
+    }
+    const inputNameElement = document.querySelector('.inputName');
+    const inputRoleElement = document.querySelector('.inputRole');
 
-  const name = inputNameElement.value;
-  const position = inputPositionElement.value;
+    const name = inputNameElement.value;
+    const role = inputRoleElement.value;
 
-  if (name && position) {
-    chrome.runtime.sendMessage({
-      type: 'register',
-      options: {
+    if (!!name && !!role) {
+      const options = {
         name,
-        position,
+        role,
         isOnline: true,
-      },
-    });
-  }
+      };
+      chrome.runtime.sendMessage({
+        type: 'REGISTER',
+        options,
+      });
+      chrome.storage.sync.set({ wilSupportStaff: options });
+      render();
+    }
+  });
 }
+
+function reRegister(event) {
+  event.preventDefault();
+  chrome.storage.sync.remove('wilSupportStaff');
+  chrome.runtime.sendMessage({ type: 'RE_REGISTER' });
+  render();
+}
+
+function render() {
+  chrome.storage.sync.get('wilSupportStaff', ({ wilSupportStaff: staff }) => {
+    const options = [
+      { role: '', text: 'Chọn vai trò' },
+      { role: 'leader', text: 'Leader' },
+      { role: 'staff', text: 'Staff' },
+    ];
+    const optionsHtml = options
+      .map(
+        option =>
+          `<option value="${option.role}" ${
+            !!staff && option.role === staff.role ? 'selected' : ''
+          }>${option.text}</option>`
+      )
+      .join(' ');
+    const html = `
+      <header class="head">
+        <span>Register</span>
+      </header>
+      <form id="form" class="form">
+        <div class="form__row grid">
+          <label class="form__label">Name</label>
+          <input
+            class="form__input inputName"
+            type="text"
+            placeholder="Nhập họ tên..."
+            value="${!!staff && staff.name ? staff.name : ''}"
+            required
+          />
+        </div>
+        <div class="form__row grid">
+          <label class="form__label" for="">Role</label>
+          <select name="" class="form__input inputRole" required>
+            ${optionsHtml}
+          </select>
+        </div>
+        <div class="form__row">
+          <button class="button form__button" type="submit">${
+            !!staff ? 'Đăng ký thành công' : 'Đăng ký'
+          }</button>
+          <a href="#" class="re-register ${
+            staff ? 'is-active' : ''
+          }">Đăng ký lại</a>
+        </div>
+      </form>
+    `;
+    rootEl.innerHTML = html;
+
+    const formElement = document.getElementById('form');
+    const reRegisterElement = document.querySelector('.re-register');
+
+    formElement.addEventListener('submit', registerSupporter);
+    reRegisterElement.addEventListener('click', reRegister);
+  });
+}
+
+window.onload = render();
